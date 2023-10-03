b82a60839f719d561e5f999391c9da8f0495550f
Lucas Gladding <lucas@me.com>
2023-10-02 21:39:04 -0400

    Added initial commit.

---
+++ Disconnected/DisconnectedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisconnectedApp.swift
+//  Disconnected
+//
+//  Created by Lucas Gladding on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct DisconnectedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ScriptForm()
+        }
+    }
+}
